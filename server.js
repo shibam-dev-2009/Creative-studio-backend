de14d962767ba66340b23261876746a0eef98257
shibam-dev-2009 <ghoshshibam009@gmail.com>
2025-12-28 14:01:23 +0530

    comi

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+app.use(cors({
+  origin: 'https://shibam-dev-2009.github.io' 
+}));
 require("dotenv").config();
 
 const app = express();
